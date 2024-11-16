2b98b7d0189624f0e89bb3f317731b772817049b
CangSpirit <cpcgskill@outlook.com>
2024-11-17 01:59:56 +0800

    feat: ✨ 支持 Maya2025

--- src/cpgui/std_imp.py
+++ src/cpgui/std_imp.py
@@ -10,40 +10,63 @@ u"""
 :bilibili: https://space.bilibili.com/351598127
 
 """
-from __future__ import unicode_literals, print_function
 try:
-    ui_lib_str = "PySide2"
-    from PySide2.QtGui import *
-    from PySide2.QtCore import *
-    from PySide2.QtWidgets import *
-    from PySide2.QtSvg import *
+    from PyQt6.QtWidgets import *
+    from PyQt6.QtCore import *
+    from PyQt6.QtGui import *
+    gui_runtime = 'PyQt6'
 except ImportError:
     try:
-        ui_lib_str = "PySide"
-        from PySide.QtGui import *
-        from PySide.QtCore import *
-        from PySide.QtSvg import *
+        from PySide6.QtGui import *
+        from PySide6.QtCore import *
+        from PySide6.QtWidgets import *
+        gui_runtime = 'PySide6'
     except ImportError:
-        ui_lib_str = "PyQt5"
-        from PyQt5.QtWidgets import *
-        from PyQt5.QtCore import *
-        from PyQt5.QtGui import *
-        from PyQt5.QtSvg import *
+        try:
+            from PyQt5.QtWidgets import *
+            from PyQt5.QtCore import *
+            from PyQt5.QtGui import *
+            gui_runtime = 'PyQt5'
+        except ImportError:
+            try:
+                from PySide2.QtGui import *
+                from PySide2.QtCore import *
+                from PySide2.QtWidgets import *
+                gui_runtime = 'PySide2'
+            except ImportError:
+                from PySide.QtGui import *
+                from PySide.QtCore import *
+                gui_runtime = 'PySide'
+
+if gui_runtime == 'PyQt6':
+    from PyQt6.QtSvgWidgets import QSvgWidget
+    from PyQt6.QtSvg import QSvgRenderer
+elif gui_runtime == 'PySide6':
+    from PySide6.QtSvgWidgets import QSvgWidget
+    from PySide6.QtSvg import QSvgRenderer
+elif gui_runtime == 'PyQt5':
+    from PyQt5.QtSvg import QSvgWidget, QSvgRenderer
+elif gui_runtime == 'PySide2':
+    from PySide2.QtSvg import QSvgWidget, QSvgRenderer
+else:
+    from PySide.QtSvg import QSvgWidget, QSvgRenderer
+
 try:
-    from shiboken2 import *
+    from shiboken6 import *
 except ImportError:
-    from shiboken import *
+    try:
+        from shiboken2 import *
+    except ImportError:
+        from shiboken import *
 
 import sys
 import maya.OpenMayaUI as OpenMayaUI
-import maya.cmds as mc
 
 deleteWidget = delete
 try:
-    if hasattr(mc, "about"):
-        ptr_t = int if sys.version_info.major > 2 else long
-        mui = wrapInstance(ptr_t(OpenMayaUI.MQtUtil.mainWindow()), QWidget)
+    if sys.version_info.major >= 3:
+        mui = wrapInstance(int(OpenMayaUI.MQtUtil.mainWindow()), QWidget)
     else:
-        mui = None
+        mui = wrapInstance(long(OpenMayaUI.MQtUtil.mainWindow()), QWidget)
 except:
     mui = None
